995c4ba450f44fd6b5468c11ae1220f8ce7b5300
vik <viktorg78@gmail.com>
2026-03-14 22:28:54 +0300

    create microrazmetka

--- www/app/js/close-modal-entry.js
+++ www/app/js/close-modal-entry.js
@@ -1,21 +1,54 @@
+const modalOverlay = document.getElementById('modal-loop');
+const closeButton = document.getElementById('close-modal-loop');
+
+const modalOverlay2 = document.getElementById('modal-add-chapter');
+const closeButton2 = document.getElementById('close-modal-add-chapter');
+
+const modalOverlay3 = document.getElementById('modal-entry');
+const closeButton3 = document.getElementById('close-modal-entry');
+
 // Закрытия модальноео окна "Авторизация"
-document.getElementById('modal-entry').addEventListener('click', function (e) {
-    if (e.target === this) {
-        document.getElementById('close-modal-entry').click()
-    }
-});
+// document.getElementById('modal-entry').addEventListener('click', function (e) {
+//     if (e.target === this) {
+//         document.getElementById('close-modal-entry').click()
+//     }
+// });
 
-document.getElementById('modal-loop').addEventListener('click', function (e) {
-    if (e.target === this) {
-        document.getElementById('close-modal-loop').click()
-    }
-});
+// document.getElementById('modal-loop').addEventListener('click', function (e) {
+//     if (e.target === this) {
+//         document.getElementById('close-modal-loop').click()
+//     }
+// });
 
-document.getElementById('modal-add-chapter').addEventListener('click', function (e) {
-    if (e.target === this) {
-        document.getElementById('close-modal-add-chapter').click()
-    }
-});
+// document.getElementById('modal-add-chapter').addEventListener('click', function (e) {
+//     if (e.target === this) {
+//         document.getElementById('close-modal-add-chapter').click()
+//     }
+// });
+
+if (modalOverlay && closeButton) {
+    modalOverlay.addEventListener('click', function(e) {
+        if (e.target === this) {
+            closeButton.click();
+        }
+    });
+}
+
+if (modalOverlay2 && closeButton2) {
+    modalOverlay2.addEventListener('click', function(e) {
+        if (e.target === this) {
+            closeButton2.click();
+        }
+    });
+}
+
+if (modalOverlay3 && closeButton3) {
+    modalOverlay3.addEventListener('click', function(e) {
+        if (e.target === this) {
+            closeButton3.click();
+        }
+    });
+}
 
 // отображения сообщений об ошибке
 function showError(message) {
